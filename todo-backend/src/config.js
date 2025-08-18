b3a7c15a02691757620d2046acd85260b636ccc9
seino <dhtp0112@gmail.com>
2025-08-18 23:01:45 +0900

    fix: move todo front,back folders to todo-app folder

--- todo-backend/src/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-dotenv.config();
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('✅MongoDB connected'))
-  .catch(err => console.error('❌MongoDB error', err));
